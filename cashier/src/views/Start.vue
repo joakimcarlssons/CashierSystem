b15fda8876559e9fcaa7ba2dc94929fdb8683316
Joakim Carlsson <hej@joakimcarlsson.se>
2021-02-23 10:12:24 +0100

    Landning page first draft

--- cashier/src/views/Start.vue
+++ cashier/src/views/Start.vue
@@ -4,12 +4,18 @@
       <h1>
           Att sälja saker har aldrig varit lättare.
       </h1>
+    
+    <div class="inputContent">
+      <input type="text" placeholder="Telefonnummer" />
 
-      <button>
-          <div class="btnContent">
-              Kom igång
-          </div>
+      <button class="cta">
+              <p>Skapa kassa</p>
+              <p class="end">></p>
       </button>
+    </div>
+
+    <h4>Lär mig mer</h4>
+
   </div>
 </template>
 
@@ -20,5 +26,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.container {
+
+    justify-content: space-evenly;
+    margin: 0 2rem;
+    text-align: center;
+
+    .cta {
+        padding: 0;
+        margin: 2rem;
+        display: flex;
+    
+        p:first-of-type {
+            align-self: center;
+            padding: 1.5rem 3rem 1.5rem 5rem;
+        }
+    
+        p:last-of-type {
+            align-self: center;
+            padding-right: 2rem;
+        }
+    }
+
+    h4 {
+        margin: 2rem;
+    }
+
+    input {
+        margin-top: 4rem;
+    }
+}
+
 
 </style>
